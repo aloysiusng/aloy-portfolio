5c8135c4435db02bfc6ff85db88d79f342f427ed
Aloysius <aloysiusng5@gmail.com>
2024-01-25 17:57:25 +0800

    contact page done

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,3 +1,44 @@
+"use client";
+import { Github, Linkedin, Mail } from "lucide-react";
+import Link from "next/link";
+
+const socials = [
+  {
+    icon: <Linkedin size={45} />,
+    href: "https://www.linkedin.com/in/aloysiusng23/",
+    label: "LinkedIn",
+    handle: "Aloysius Ng",
+  },
+  {
+    icon: <Mail size={45} />,
+    href: "mailto:aloysiusng5@gmail.com",
+    label: "Email",
+    handle: "aloysiusng5@gmail.com",
+  },
+  {
+    icon: <Github size={45} />,
+    href: "https://github.com/aloysiusng",
+    label: "GitHub",
+    handle: "aloysiusng",
+  },
+];
 export default function Page() {
-  return <h1>Hello, projects.js!</h1>;
+  return (
+    <div >
+      <div className="grid w-full grid-cols-1 gap-8 mx-auto mt-32 sm:mt-0 sm:grid-cols-3 lg:gap-16">
+        {socials.map((s) => (
+          <div key={s.href}>
+            <Link href={s.href} target="_blank" className="p-4 relative flex flex-col items-center gap-4 duration-700 group md:gap-8 md:py-24  lg:pb-48  md:p-16">
+              <span className="absolute w-px h-2/3 bg-gradient-to-b from-zinc-500 via-zinc-500/50 to-transparent" aria-hidden="true" />
+              <span className="relative z-10 flex items-center justify-center w-20 h-20 text-sm duration-1000 border rounded-full text-zinc-200 group-hover:text-white group-hover:bg-zinc-900 border-zinc-500 bg-zinc-900 group-hover:border-zinc-200 drop-shadow-orange">{s.icon}</span>
+              <div className="z-10 flex flex-col items-center">
+                <span className="lg:text-xl font-medium duration-150 xl:text-3xl text-zinc-200 group-hover:text-white font-display">{s.handle}</span>
+                <span className="mt-4 text-sm text-center duration-1000 text-zinc-400 group-hover:text-zinc-200">{s.label}</span>
+              </div>
+            </Link>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
