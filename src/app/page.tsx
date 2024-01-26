58827dfc6296f4e313577202bf49420ad2512b9a
Aloysius <aloysiusng5@gmail.com>
2024-01-27 00:39:11 +0800

    update image size

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,7 +14,7 @@ export default function Home() {
       </div>
       <br></br>
       <div className="my-3">
-        <Image src="/aloy.png" width={500} height={500} alt="Aloysius" />
+        <Image src="/aloy.png" width={300} height={300} alt="Aloysius" />
       </div>
       <div className="my-5">
         <p className="px-5 text-xl text-slate-400 font-mono text-pretty text-center">🚀 Welcome to my digital space! I am Aloysius Ng, a highly motivated and results-driven software engineer.</p>
