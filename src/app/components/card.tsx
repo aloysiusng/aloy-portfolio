514679559db06794536789c73e065737300114c7
Aloysius <aloysiusng5@gmail.com>
2024-01-27 00:45:40 +0800

    remove comments

--- src/app/components/card.tsx
+++ src/app/components/card.tsx
@@ -66,20 +66,6 @@ export default function Card({ header = "", summary = "", content = "", company 
             </p>
           </div>
         </div>
-
-        // <div id="modal" tabIndex={-1} aria-hidden="true" className="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
-        //   <div className="relative p-4 w-full max-w-2xl max-h-full">
-        //     <div className="relative bg-white rounded-lg shadow dark:bg-gray-700">
-        //       <div className="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600">
-        //         <h3 className="text-xl font-semibold text-gray-900 dark:text-white">{header}</h3>
-        //       </div>
-
-        //       <div className="p-4 md:p-5 space-y-4">
-        //         <p className="text-base leading-relaxed text-gray-500 dark:text-gray-400">{content}</p>
-        //       </div>
-        //     </div>
-        //   </div>
-        // </div>
       )}
     </>
   );
